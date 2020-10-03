1a5b562ac94e4ee2ee9a5fbfa9f0f977774531a8
charis117 <manyac4@gmail.com>
2020-10-03 07:03:48 +0300

    basic list with titles

--- app/src/main/java/gr/chris/pada/MainActivity.java
+++ app/src/main/java/gr/chris/pada/MainActivity.java
@@ -3,44 +3,52 @@ package gr.chris.pada;
 import android.app.*;
 import android.os.*;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.jsoup.Jsoup;
 import java.io.IOException;
 import android.widget.Toast;
 import android.widget.TextView;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 		Thread th=new Thread(new Runnable(){
-			
+
 			public void run(){
 				try{
 					Document wb=Jsoup.connect("https://eee.uniwa.gr/el/anakinoseis/anakoinoseis-grammateias").get();
 					System.out.println(wb.title());
 					Elements els=wb.getElementsByTag("article");
-					int size =els.size();
+					final StringBuilder fTex=new StringBuilder();
+					for(Element ei:els) {
+						final String timeString=ei.getElementsByTag("time").html();
+						final String timeSpampString=ei.getElementsByTag("time").attr("datetime");
+				        String title=ei.getElementsByTag("itemprop").get(0).html();
+				        fTex.append(timeString);
+				        fTex.append(":");
+				        fTex.append(title);
+				        fTex.append("\n");
+				        int b=5;
 
-					final String timeString=els.get(0).getElementsByTag("time").html();
-					final String timeSpampString=els.get(0).getElementsByTag("time").attr("datetime");
-					System.out.println(timeSpampString);
+					}
+
 					runOnUiThread(new Runnable(){
 						public void run(){
 							TextView tv=(TextView)findViewById(R.id.mainTextView);
-							tv.setText(timeString);
-							Toast.makeText(getApplicationContext(),timeSpampString,Toast.LENGTH_SHORT).show();
+							tv.setText(fTex.toString());
 						}
 					});
 				}catch(IOException e){}
 
-				
-		
-		}
-		
+
+
+			}
+
 		});
-    }
+	}
 }
